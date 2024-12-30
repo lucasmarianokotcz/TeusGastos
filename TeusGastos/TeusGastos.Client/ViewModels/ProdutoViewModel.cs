94a63729fe9f3af580f710595f957f35534c65de
lucasmarianokotcz <lucasmariano-@hotmail.com>
2024-12-30 06:34:29 -0300

    melhorias (descrição dos campos, ordenação das unidades de medida no cadastro de produtos)

--- TeusGastos/TeusGastos.Client/ViewModels/ProdutoViewModel.cs
+++ TeusGastos/TeusGastos.Client/ViewModels/ProdutoViewModel.cs
@@ -7,16 +7,19 @@ public class ProdutoViewModel
 {
     public int Id { get; set; }
 
+    [Display(Name = "Descrição")]
     [Required(ErrorMessage = "O campo {0} é obrigatório.")]
     [StringLength(Produto.DescricaoMaxLength, ErrorMessage = "O campo {0} precisa ter no máximo {1} caracteres.")]
     public string Descricao { get; set; } = string.Empty;
     
+    [Display(Name = "Código EAN")]
     [Required(ErrorMessage = "O campo {0} é obrigatório.")]
     [StringLength(Produto.CodigoEANLength,
         MinimumLength = Produto.CodigoEANLength,
         ErrorMessage = "O campo {0} precisa ter exatamente {1} caracteres.")]
     public string CodigoEAN { get; set; } = string.Empty;
 
-    [Required(ErrorMessage = "Selecione uma Unidade de Medida")]
+    [Display(Name = "Unidade de Medida")]
+    [Required(ErrorMessage = "Selecione uma {0}.")]
     public string? UnidadeMedida { get; set; }
 }
